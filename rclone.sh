551591aa36b24621593e7cc0b864ed0272b255ae
Pavithran <pavithr29@gmail.com>
2021-06-29 23:23:05 +0530

    Added rclone.conf env

--- rclone.sh
+++ rclone.sh
@@ -1,4 +1,9 @@
 #! /bin/bash -eu
+if [[ "$RCLONE_CONFIG_GIST" != "" ]]; then
+    echo "Rclone config detected"
+    curl $RCLONE_CONFIG_GIST -o /app/conf/rclone.conf
+    echo "Rclone config added"
+fi
 
 if [ "$ENABLE_RCLONE" = "true" ]; then
   echo "Start Rclone, please make sure you can connect to Github website. if not, please set docker env ENABLE_RCLONE=false"
